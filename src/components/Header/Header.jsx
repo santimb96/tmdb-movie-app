4f07c4f0cf2380e106436d8ee4218b079c78dfc6
santimb96 <santiagomartinezbota@gmail.com>
2023-02-21 18:28:20 +0100

    do header and search bar

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import SearchBar from '../SearchBar/SearchBar'
+import styles from './Header.module.css'
+const Header = () => {
+  return (
+    <>
+      <div className={styles.header}>
+        <div className={styles.content}>
+          <img src="../../../public/img/tmdbLogo.svg" alt="tmdbLogo" />
+          <SearchBar />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Header
---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import { AiOutlineSearch } from 'react-icons/ai'
+import styles from './SearchBar.module.css'
+
+const SearchBar = ({ field = 'title' }) => {
+  const [search, setSearch] = useState('')
+
+  const onSearch = () => {
+    console.warn(search)
+    setSearch('')
+  }
+
+  return (
+    <>
+      {console.log(search)}
+      <div className={styles.searchComponent}>
+        <input
+          placeholder={`Search by ${field}`}
+          className={styles.searchBar}
+          type="text"
+          value={search}
+          id={field}
+          name={field}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <button className={styles.searchButton} onClick={() => onSearch()}>
+          <AiOutlineSearch />
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default SearchBar

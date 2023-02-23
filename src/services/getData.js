8b8a4335ee54d36031d55629b0c8020a1911f29f
santimb96 <santiagomartinezbota@gmail.com>
2023-02-23 01:43:27 +0100

    do get data by page

--- src/services/getData.js
+++ src/services/getData.js
@@ -1,8 +1,9 @@
 const getFilms = (page = 1) => {
+  console.log('page', page)
   return fetch(
     `${import.meta.env.VITE_API_URL}/discover/movie?api_key=${
       import.meta.env.VITE_API_KEY
-    }&language=en-US&sort_by=popularity.desc&page=${page}}`,
+    }&language=en-US&sort_by=popularity.desc&page=${page}`,
   )
     .then((response) => response.json())
     .then((data) => data)

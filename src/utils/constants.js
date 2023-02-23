e78b326304ed91206f8fbac636851c7ba430882f
santimb96 <santiagomartinezbota@gmail.com>
2023-02-23 01:11:18 +0100

    add some colors to vote average field

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -15,16 +15,24 @@ const MONTHS = [
 
 const COLORS = [
   {
+    average: (n) => n === 0,
+    color: 'gray',
+  },
+  {
     average: (n) => n < 5,
     color: 'red',
   },
   {
-    average: (n) => n >= 5 && n < 7,
+    average: (n) => n >= 5 && n < 6,
     color: 'yellow',
   },
   {
-    average: (n) => n >= 7,
+    average: (n) => n >= 6 && n < 7,
     color: 'yellowgreen',
+  },
+  {
+    average: (n) => n >= 7,
+    color: 'var(--secondary-color)',
   },
 ]
 
